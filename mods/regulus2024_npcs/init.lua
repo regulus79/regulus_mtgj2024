6d40289c552c6a3a8ff675052fb2b306c3356afd
regulus79 <117475203+regulus79@users.noreply.github.com>
2024-11-07 23:19:23 -0500

    Npc model and more behavior simepl stuff

--- mods/regulus2024_npcs/init.lua
+++ mods/regulus2024_npcs/init.lua
@@ -7,12 +7,41 @@ minetest.register_chatcommand("spawn_npc", {
 })
 
 
+local update_head_direction = function(self, target_pos, interpolation, exponential_interpolation_dtime)
+    local relative_pos = (target_pos - self.object:get_pos()):rotate(-self.object:get_rotation())
+    local up = vector.rotate(vector.new(0, 1, 0), self.object:get_rotation())
+    up = -up:cross(relative_pos):cross(relative_pos):normalize()
+    local rotation = -vector.dir_to_rotation(relative_pos, up)
+    -- Add bounds to yaw
+    rotation.y = math.min(math.max(-math.pi/2, rotation.y), math.pi/2)
+    if exponential_interpolation_dtime > 0 then
+        local current_rotation = self.object:get_bone_override("Head").rotation.vec
+        self.object:set_bone_override("Head", {
+            rotation = {vec = current_rotation + (rotation - current_rotation) * (interpolation) * exponential_interpolation_dtime}, -- aaa need to do the dtime right
+        })
+    else
+        self.object:set_bone_override("Head", {
+            rotation = {vec = rotation, interpolation = interpolation},
+        })
+    end
+end
+
+
+local get_leg_back_and_forth = function(t)
+    t = t - math.floor(t)
+    if t < 0.5 then
+        return t
+    else
+        return 0.5 - (t - 0.5)
+    end
+end
+
 
 local register_npc = function(name, def)
     minetest.register_entity(name, {
         initial_properties = {
             visual = def.visual or "mesh",
-            mesh = def.mesh or "regulus2024_player2.b3d",
+            mesh = def.mesh or "regulus2024_player2.glb",
             physical = def.physical or true,
             collide_with_objects = def.collide_with_objects or true,
             collisionbox = def.collisionbox or {-0.3, 0, -0.3, 0.3, 1.8, 0.3},
@@ -21,16 +50,131 @@ local register_npc = function(name, def)
             visual_size = def.collide_with_objects or vector.new(1, 1, 1),
             textures = def.collide_with_objects or {"regulus2024_player_template.png"},
             use_texture_alpha = def.collide_with_objects or true,
+            automatic_face_movement_dir = -90,
+            automatic_face_movement_max_rotation_per_sec = 360
             --- PLEAE ADD MORE
         },
-        on_step = function(self, dtime)
+        _state = "idle",
+        _look_target = vector.new(0, 0, 1),
+        _average_time_per_look_update = 1,
+        _lose_notice_dist = 8,
+        _gain_notice_dist = 4,
+        _walk_target = nil,
+        _target_velocity = nil,
+        _average_time_per_walk_target_update = 5,
+        _walk_speed = 2,
+        _min_speed = 0.5,
+        _max_speed = 3,
+        _walk_cycle_position = 0,
+        _stride_length = 1,
+        _leg_length = 0.6985,
+        on_step = def.on_step or function(self, dtime)
+
+            -- LOOK DIRECTION
+
+            if self._state == "idle" then
+                if type(self._look_target) == "userdata" and self._look_target:get_pos():distance(self.object:get_pos()) > self._lose_notice_dist then
+                    self._look_target = nil
+                end
+                for _, player in pairs(minetest.get_connected_players()) do
+                    if player:get_pos():distance(self.object:get_pos()) < self._gain_notice_dist then
+                        self._look_target = player
+                    end
+                end
+            end
+            if type(self._look_target) == "userdata" then
+                update_head_direction(self, self._look_target:get_pos(), 8, dtime)
+            else
+                update_head_direction(self, self._look_target, 2, dtime) 
+                -- If not focused on a player, pick a new target every so often
+                if math.random() < 1 / self._average_time_per_look_update * dtime then
+                    self._look_target = self.object:get_pos() + vector.new(math.random() * 5 - 2.5, math.random() - 0.5, math.random() * 10 + 2):rotate(self.object:get_rotation())
+                end
+            end
+
+            -- WALKING
+
+            if self._state == "idle" then
+                if math.random() < dtime / self._average_time_per_walk_target_update then
+                    self._walk_target = self.object:get_pos() + vector.new(math.random() - 0.5, math.random() - 0.5, math.random() - 0.5) * 10
+                end
+            end
+
+            if self._walk_target then
+                self._target_velocity = (self._walk_target - self.object:get_pos()):normalize() * self._walk_speed
+            end
+
+            -- APPLY MOVEMENT
+
+            local new_vel = self._target_velocity or vector.new(0,0,0)
+            new_vel = new_vel:normalize() * math.min(self._max_speed, new_vel:length())
+            new_vel.y = self.object:get_velocity().y
+            self.object:set_velocity(new_vel)
+
+            -- DO LEG ANIMATION
+            -- very simple stuff here, I want to work on a beautiful physics-based foot placement system in the future for other games, but for now this will ahve to do.
+            local horizontal_vel = self.object:get_velocity()
+            horizontal_vel.y = 0
+
+            if horizontal_vel:length() > self._min_speed then
+                self._walk_cycle_position = self._walk_cycle_position + dtime * horizontal_vel:length() / self._stride_length / self._leg_length / 2
+
+                local angle_left = (get_leg_back_and_forth(self._walk_cycle_position) - 0.25) * math.pi
+                local angle_right = (get_leg_back_and_forth(self._walk_cycle_position + 0.5) - 0.25)* math.pi
+
+                self.object:set_bone_override("LegL", {
+                    rotation = {vec = vector.new(angle_left, 0, 0), absolute = true}
+                })
+                self.object:set_bone_override("LegR", {
+                    rotation = {vec = vector.new(angle_right, 0, 0), absolute = true}
+                })
+                self.object:set_bone_override("ArmL", {
+                    rotation = {vec = vector.new(angle_right/2, 0, 0), absolute = true}
+                })
+                self.object:set_bone_override("ArmR", {
+                    rotation = {vec = vector.new(angle_left/2, 0, 0), absolute = true}
+                })
+            else
+                self.object:set_velocity(vector.new(0, self.object:get_velocity().y, 0)) --- BAd idea, not good, migth want to use vel in other code
+                self.object:set_bone_override("LegL", {
+                    rotation = {vec = vector.new(0, 0, 0), absolute = true, interpolation = 0.25}
+                })
+                self.object:set_bone_override("LegR", {
+                    rotation = {vec = vector.new(0, 0, 0), absolute = true, interpolation = 0.25}
+                })
+                self.object:set_bone_override("ArmL", {
+                    rotation = {vec = vector.new(0, 0, 0), absolute = true, interpolation = 0.25}
+                })
+                self.object:set_bone_override("ArmR", {
+                    rotation = {vec = vector.new(0, 0, 0), absolute = true, interpolation = 0.25}
+                })
+            end
+
+            if def.extra_on_step then
+                def.extra_on_step(self, dtime)
+            end
         end,
-        on_rightclick = def.on_rightclick
+        on_rightclick = def.on_rightclick or function(self, clicker)
+            self._look_target = clicker
+            
+            if def.extra_on_rightclick then
+                def.extra_on_rightclick(self, clicker)
+            end
+        end
     })
 end
 
 register_npc("regulus2024_npcs:testnpc", {
-    on_rightclick = function(self, clicker)
+    extra_on_rightclick = function(self, clicker)
         regulus2024_dialogue.start_dialogue(clicker, "dni1")
     end
 })
+
+
+--[[
+For the on-step
+every random length of time, update look target
+every random length of time, update movement target (pathfinding or nay)?
+- How about pick a target position/player based on feeling?
+
+]]
